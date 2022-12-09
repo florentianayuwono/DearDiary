95f90884d9a7c525616ba071a3e7381d53313497
florentianayuwono <e0851439@u.nus.edu>
2022-12-10 00:38:01 +0800

    Set up main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
-import { Route, Routes } from 'react-router-dom';
-import { Recent, Searchbar, Sidebar } from './components';
-import { Discover, Diary, Search } from './pages';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Recent, Searchbar, Sidebar } from "./components";
+import { Discover, Diary, Search } from "./pages";
 // import './App.css';
 
 function App() {
   return (
     <div className="relative flex">
       <Sidebar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-lightPink to-mediumPink">
         <Searchbar />
-
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
-            <Routes>
-              <Route path="/" element={<Discover />} />
-              <Route path="/diary" element={<Diary />} />
-              <Route path="/search/:searchTerm" element={<Search />} />
-            </Routes>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Discover />} />
+                <Route path="/diary" element={<Diary />} />
+                <Route path="/search/:searchTerm" element={<Search />} />
+              </Routes>
+            </BrowserRouter>
           </div>
           <div className="xl:sticky relative top-0 h-fit">
             <Recent />
           </div>
-
+        </div>
       </div>
-      </div>
-      </div>
- 
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,17 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./*.{tsx,ts,html}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightYellow: "#FFF5E4",
+        lightPink: "#FFE3E1",
+        mediumPink: "#FFD1D1",
+        deepPink: "#FF9494"
+      }
+    },
   },
   plugins: [],
 }
