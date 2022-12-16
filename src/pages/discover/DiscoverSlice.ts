5e0fa82baa8e005b400c586f68c78133d68c9bb4
florentianayuwono <e0851439@u.nus.edu>
2022-12-16 20:11:47 +0800

    Add tailwind styling

---
+++ src/pages/discover/DiscoverSlice.ts
@@ -0,0 +1,54 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+
+export interface DiscoverState {
+    url: string
+}
+
+const initialState: DiscoverState = {
+    url: "https://deardiary.onrender.com"
+};
+
+// export const incrementAsync = createAsyncThunk(
+//     'counter/fetchCount',
+//     async (amount: number) => {
+//       const response = await fetchCount(amount);
+//       // The value we return becomes the `fulfilled` action payload
+//       return response.data;
+//     }
+//   );
+  
+  export const discoverSlice = createSlice({
+    name: 'discover',
+    initialState,
+    // The `reducers` field lets us define reducers and generate associated actions
+    reducers: {
+        // action = {type: "", payload: ---}
+        reducer: (state, action) => {
+            switch(action.type) {
+                default:
+                    return state
+            }
+        }
+    }
+  });
+  
+  export const { reducer } = discoverSlice.actions;
+  
+  // The function below is called a selector and allows us to select a value from
+  // the state. Selectors can also be defined inline where they're used instead of
+  // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+  export const selectCount = (state: RootState) => state.counter.value;
+  
+  // We can also write thunks by hand, which may contain both sync and async logic.
+  // Here's an example of conditionally dispatching actions based on current state.
+  export const incrementIfOdd =
+    (amount: number): AppThunk =>
+    (dispatch, getState) => {
+      const currentValue = selectCount(getState());
+      if (currentValue % 2 === 1) {
+        dispatch(incrementByAmount(amount));
+      }
+    };
+  
+  export default counterSlice.reducer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Diary } from "./Diary";
-import { Discover } from "./Discover";
+import { Discover } from "./discover/Discover";
 import { Search } from "./Search";
 
 export {
